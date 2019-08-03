1edde255992a2d7d6cf8604ebd9fb3bd9e57a305
lig001 <lig001@cmrh.com>
2019-08-03 20:34:12 +0800

    add zuul facade

--- nourish-service/record/src/main/java/org/liguang/record/controller/RecordController.java
+++ nourish-service/record/src/main/java/org/liguang/record/controller/RecordController.java
@@ -1,5 +1,8 @@
 package org.liguang.record.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.support.BindingAwareModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +16,14 @@ import java.util.Map;
 @RestController
 public class RecordController {
 
+    @Autowired
+    private Environment environment;
+
     @RequestMapping("/record")
     public Map<String, Object> doRecord(@RequestBody Map<String, Object> param) {
         param.put("record", "The action has been recorded!");
+        param.put("instancePort", environment.getProperty("local.server.port"));
+
         return param;
     }
 }
